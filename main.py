09ad9b7f5da3bbc5a37ddf5b2a784be05749a25c
Elias <elias.alhomsi@mail.mcgill.ca>
2018-09-29 17:32:35 -0400

    fixing stuff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
+from training import train_set, get_result
+import imp
 
 # emulated camera
 from webcamvideostream import webcamvideostream
@@ -7,6 +9,7 @@ from webcamvideostream import webcamvideostream
 import cv2
 
 app = Flask(__name__)
+clean_face = imp.load_source('extractFaces', 'initcode/extractFaces.py').clean_face
 
 @app.route('/')
 def index():
@@ -18,16 +21,18 @@ def gen(camera):
     """Video streaming generator function."""
     while True:
         frame = camera.read()
+        frame = clean_face(frame)
         ret, jpeg = cv2.imencode('.jpg', frame)
 
         # print("after get_frame")
         if jpeg is not None:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+            yield make_frame(jpeg)
         else:
             print("frame is none")
 
-
+def make_frame(jpeg):
+    return (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
@@ -37,4 +42,8 @@ def video_feed():
 
 
 if __name__ == '__main__':
+    # start with training the set
+    #train_set()
+
+    # Run
     app.run(host='0.0.0.0', port=5010, debug=True, threaded=True)
